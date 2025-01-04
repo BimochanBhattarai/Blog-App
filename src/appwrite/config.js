5819e6567b251706aa9cbf77b0b1303591bf87f2
BimochanBhattarai <itsbimochan@gmail.com>
2025-01-05 00:34:29 +0545

    create deletePost method in config.js file

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -16,42 +16,57 @@ export class Service {
 		this.bucket = new Storage(this.client);
 	}
 
-    async createPost({title, slug, content, featuredImage, status, userId}) { // If we have to change database again, we will just have to change te below setting, the method will be the same !!!
-        try {
-            return await this.databases.createDocument(
-                conf.appwriteDatabaseId,
-                conf.appwriteCollectionId,
-                slug, // Also can use ID.Unique()
-                {
-                    title,
-                    content,
-                    featuredImage,
-                    status,
-                    userId
-                }
-            ) 
-        } catch (error) {
-            console.log("Appwrite servie :: createPost :: error ", error);
-        }
-    }
+	async createPost({ title, slug, content, featuredImage, status, userId }) {
+		// If we have to change database again, we will just have to change te below setting, the method will be the same !!!
+		try {
+			return await this.databases.createDocument(
+				conf.appwriteDatabaseId,
+				conf.appwriteCollectionId,
+				slug, // Also can use ID.Unique()
+				{
+					title,
+					content,
+					featuredImage,
+					status,
+					userId,
+				}
+			);
+		} catch (error) {
+			console.log("Appwrite servie :: createPost :: error ", error);
+		}
+	}
 
-    async updatePost(slug, {title, content, featuredImage, status}) {
-        try {
-            return await this.databases.updateDocument(
-                conf.appwriteDatabaseId,
-                conf.appwriteCollectionId,
-                slug,
-                {
-                    title,
-                    content,
-                    featuredImage,
-                    status
-                }
-            )
-        } catch (error) {
-            console.log("Appwrite servie :: updatePost :: error ", error)    
-        }
-    }
+	async updatePost(slug, { title, content, featuredImage, status }) {
+		try {
+			return await this.databases.updateDocument(
+				conf.appwriteDatabaseId,
+				conf.appwriteCollectionId,
+				slug,
+				{
+					title,
+					content,
+					featuredImage,
+					status,
+				}
+			);
+		} catch (error) {
+			console.log("Appwrite servie :: updatePost :: error ", error);
+		}
+	}
+
+	async deletePost(slug) {
+		try {
+			await this.databases.deleteDocument(
+				conf.appwriteDatabaseId,
+				conf.appwriteCollectionId,
+				slug
+			);
+			return true;
+		} catch (error) {
+			console.log("Appwrite servie :: deletePost :: error", error);
+			return false;
+		}
+	}
 }
 
 const service = new Service();
