f3896c7ab16b7c74348d73e8a29a1561a4f4e22e
BimochanBhattarai <itsbimochan@gmail.com>
2025-01-05 02:16:42 +0545

    Initial Redex Setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux'
 import authService from './appwrite/auth'
 import {login,logout} from './store/authSlice'
+import { Footer, Header } from './components';
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
@@ -20,9 +22,17 @@ function App() {
     .finally(()=> setLoading(false))
   },[])
 
-  return !loading ? (
-    <div>
+  
 
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+        TODO: <Outlet/>
+        </main>
+        <Footer/>
+      </div>
     </div>
   ) : (
     <div>
