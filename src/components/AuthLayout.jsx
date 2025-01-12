9923651f88a9f83b18b84c85c41502bd6bb0b69e
BimochanBhattarai <itsbimochan@gmail.com>
2025-01-12 20:30:31 +0545

    Add AuthLayout component to protect routs

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+export default Protected = ({children, authentication = true}) => {
+    const navigate = useNavigate();
+    const [loader, setLoader] = useState(true);
+    const authStatus = useSelector(state => state.auth.status);
+
+    useEffect(() => {
+        //TODO: Make it more easy to understand
+
+        // if(authStatus === true){
+        //     navigate('/');
+        // } else if(authStatus === false){
+        //     navigate('/login');
+        // }
+
+        if(authentication && authStatus !== authentication) {
+            navigate('/login');
+        }
+        else if(!authentication && authStatus === 'authenticated') {
+            navigate('/');
+        }
+        setLoader(false);
+    }, [authStatus, navigate, authentication]);
+
+  return loader ? <div>Loading...</div> : <div>{children}</div>;
+}
+
