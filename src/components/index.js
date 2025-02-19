b946e47c87e501aaa08f45ecb9a8f9fc2ca3dc8e
BimochanBhattarai <itsbimochan@gmail.com>
2025-02-19 21:12:23 +0545

    update index for import of components

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,23 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./Container/Container"
+import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Post from "./Post";
+import PostFrom from "./Post-Form/PostForm";
 
-export { Header, Footer, Container, Logo, LogoutBtn, RTE };
+export {
+	Header,
+	Footer,
+	Container,
+	Logo,
+	LogoutBtn,
+	RTE,
+	Signup,
+	Login,
+	Post,
+	PostFrom,
+};
